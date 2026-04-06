db24d4f661b73bcd649d4101ca5891d0a86b8203
Hrithik Burnwal <hrithikrocks124@gmail.com>
2026-04-06 22:18:33 +0530

    feat: create extractJson utility file to freshenup the response

--- server/routes/user.routes.js
+++ server/routes/user.routes.js
@@ -1,9 +1,10 @@
 import express from "express";
-import { getCurrentUser } from "../controllers/user.controllers.js";
+import { generatedemo, getCurrentUser } from "../controllers/user.controllers.js";
 import isAuth from "../middlewares/isAuth.js";
 
 const userRouter = express.Router();
 
 userRouter.get("/me", isAuth, getCurrentUser)
+userRouter.get("/gen", generatedemo)
 
 export default userRouter
