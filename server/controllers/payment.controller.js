b3ce0ba9926e5bb3ba0145d81fa6cb62b5bd5d3c
Hrithik Burnwal <hrithikrocks124@gmail.com>
2026-04-10 14:50:30 +0530

    feat: implement payment processing with createOrder and verifyPayment functions

---
+++ server/controllers/payment.controller.js
@@ -0,0 +1,84 @@
+import razorpay from "../config/razorpay.services.js";
+import Payment from "../models/payment.model.js";
+import crypto from "crypto";
+
+export const createOrder = async (req, res) => {
+  try {
+    const { planId, amount, credits } = req.body;
+    if (!amount || !credits) {
+      return res
+        .status(400)
+        .json({ message: "Amount and credits are required" });
+    }
+
+    const options = {
+      amount: amount * 100, // Convert to paise
+      currency: "INR",
+      receipt: `receipt_${Date.now()}`,
+    };
+
+    const order = await razorpay.orders.create(options);
+
+    await Payment.create({
+      userId: req.user._id,
+      planId,
+      amount,
+      credits,
+      razorpayOrderId: order.id,
+      status: "created",
+    });
+
+    res.json(order);
+  } catch (error) {
+    console.error("Error creating payment:", error);
+    res.status(500).json({ message: "Internal server error" });
+  }
+};
+
+export const verifyPayment = async (req, res) => {
+  try {
+    const { razorpay_payment_id, razorpay_order_id, razorpay_signature } =
+      req.body;
+
+    const body = razorpay_order_id + "|" + razorpay_payment_id;
+    const expectedSignature = crypto
+      .createHmac("sha256", process.env.RAZORPAY_SECRET_KEY)
+      .update(body)
+      .digest("hex");
+
+    if (expectedSignature !== razorpay_signature) {
+      return res.status(400).json({ message: "Invalid payment signature" });
+    }
+
+    const payment = await Payment.findOne({
+      razorpayOrderId: razorpay_order_id,
+    });
+
+    if (!payment) {
+      return res.status(404).json({ message: "Payment not found" });
+    }
+
+    if (payment.status === "paid") {
+      return res.status(400).json({ message: "Payment already verified" });
+    }
+
+    payment.status = "paid";
+    payment.razorpayPaymentId = razorpay_payment_id;
+    await payment.save();
+
+    const updateUser = await User.findByIdAndUpdate(
+      payment.userId,
+      { $inc: { credits: payment.credits } },
+      { new: true },
+    );
+
+    res.json({
+      success: true,
+      message: "Payment verified & credits added successfully",
+      credits: updateUser.credits,
+    });
+  } catch (error) {
+    console.error("Error verifying payment:", error);
+    res.status(500).json({ message: "Internal server error" });
+  }
+};
