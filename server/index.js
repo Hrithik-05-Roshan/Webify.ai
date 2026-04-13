94efa5137f37f81254b968f5388fed7a9dfaa879
Hrithik Burnwal <hrithikrocks124@gmail.com>
2026-04-13 16:53:23 +0530

    Update CORS origin to production URL

--- server/index.js
+++ server/index.js
@@ -16,7 +16,7 @@ const PORT=process.env.PORT || 5000
 app.use(express.json())
 app.use(cookieParser())
 app.use(cors({
-    origin:"http://localhost:5173",
+    origin:"https://webify-ai-flame.vercel.app/",
     credentials:true
 }))
 app.use("/api/auth", authRouter)
